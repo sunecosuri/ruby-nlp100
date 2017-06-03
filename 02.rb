7398596c14115518fb5090d07f2affd7c2ba22f9
sunecosurisan <sunecosuri-san@sunecosurisan-no-MacBook-Air.local>
2017-06-04 06:41:25 +0900

    メソッドの修正

--- 02.rb
+++ 02.rb
@@ -2,8 +2,7 @@ patrol_car = "パトカー"
 taxi = "タクシー"
 
 def joinStrAlternate(text_a,text_b)
-  x = []
-  p text_a.chars.zip(text_b.chars).join
+  return text_a.chars.zip(text_b.chars).join
 end
 
-joinStrAlternate(patrol_car,taxi)
+p joinStrAlternate(patrol_car,taxi)
