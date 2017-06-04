8bf75b9873d8221a26982b4b2f9689bda862aec1
Kouki Narumi <sunecosuri@gmail.com>
2017-06-04 21:29:20 +0900

    Update 02.rb

--- 02.rb
+++ 02.rb
@@ -1,9 +1,8 @@
-patrol_car = "パトカー"
-taxi = "タクシー"
-
-def joinStrAlternate(text_a,text_b)
-  x = []
-  p text_a.chars.zip(text_b.chars).join
-end
-
-joinStrAlternate(patrol_car,taxi)
+  1 patrol_car = "パトカー"
+  2 taxi = "タクシー"
+  3
+  4 def joinStrAlternate(text_a,text_b)
+  5   return text_a.chars.zip(text_b.chars).join
+  6 end
+  7
+  8 p joinStrAlternate(patrol_car,taxi)
